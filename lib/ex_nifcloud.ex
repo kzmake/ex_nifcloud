75f7fd0ae628d5a113fa5834ffa4c4538be20390
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-10-08 20:28:35 +0900

    🌱 init repository

---
+++ lib/ex_nifcloud.ex
@@ -0,0 +1,18 @@
+defmodule ExNifcloud do
+  @moduledoc """
+  Documentation for ExNifcloud.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExNifcloud.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_nifcloud_test.exs
@@ -0,0 +1,8 @@
+defmodule ExNifcloudTest do
+  use ExUnit.Case
+  doctest ExNifcloud
+
+  test "greets the world" do
+    assert ExNifcloud.hello() == :world
+  end
+end
